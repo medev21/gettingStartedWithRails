b4a54412438703375593b01e6ced6adaac26e353
Martin Ben <mebenav88@gmail.com>
2015-10-31 22:01:34 -0400

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  def new
+  end
+
+  def create
+    # @article = Article.create(params.require(:articles).permit(:title))
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
+end
