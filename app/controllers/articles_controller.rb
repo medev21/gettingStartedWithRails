a2f1cef1e49e4b971588122deba2415385de3c78
Martin Ben <mebenav88@gmail.com>
2015-10-31 22:09:45 -0400

    create index.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
 
   before_action :find_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
   def new
   end
 
@@ -15,6 +19,7 @@ class ArticlesController < ApplicationController
 
   def show
   end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
