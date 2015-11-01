a9a1fd6e74406ad661613d7de242faf5aec96865
Martin Ben <mebenav88@gmail.com>
2015-11-01 01:02:50 -0400

    add security, authenticate user-basic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-
+  
   before_action :find_article, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def index
     @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   before_action :find_article
-  before_action :find_comment
+  before_action :find_comment, only: [:destroy]
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
