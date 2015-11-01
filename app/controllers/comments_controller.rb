4d69c95d3f2f4cf3de727922ef6f58f08e60d7eb
Martin Ben <mebenav88@gmail.com>
2015-11-01 00:11:52 -0400

    add create action to comment controller, add comment display to show view in articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  before_action :find_article
+  
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+    def find_article
+      @article = Article.find(params[:article_id])
+    end
+
 end
